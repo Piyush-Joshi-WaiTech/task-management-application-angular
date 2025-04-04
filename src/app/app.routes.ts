17091e7e5d0700e4e093a8ea33ff7b29eb273be9
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-04-04 11:36:56 +0530

    feat: add renderMode data to task route for client-side rendering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
+// import { Routes } from '@angular/router';
+// import { LoginComponent } from './login/login.component';
+// import { HomeComponent } from './home/home.component';
+// import { ProjectComponent } from './project/project.component';
+// import { TaskComponent } from './task/task.component';
+// import { EditProjectComponent } from './edit-project/edit-project.component';
+// import { EditTaskComponent } from './edit-task/edit-task.component';
+
+// export const appRoutes: Routes = [
+//   { path: '', redirectTo: '/login', pathMatch: 'full' },
+//   { path: 'login', component: LoginComponent },
+//   { path: 'home', component: HomeComponent },
+//   { path: 'projects', component: ProjectComponent },
+//   { path: 'tasks/:projectTitle', component: TaskComponent },
+//   { path: 'projects/edit/:id', component: EditProjectComponent },
+
+//   { path: '**', redirectTo: 'projects' },
+// ];
+
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -11,8 +30,11 @@ export const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'projects', component: ProjectComponent },
-  { path: 'tasks/:projectTitle', component: TaskComponent },
+  {
+    path: 'tasks/:projectTitle',
+    component: TaskComponent,
+    data: { renderMode: 'client' }, // ⬅️ Add this line only here
+  },
   { path: 'projects/edit/:id', component: EditProjectComponent },
-
   { path: '**', redirectTo: 'projects' },
 ];
