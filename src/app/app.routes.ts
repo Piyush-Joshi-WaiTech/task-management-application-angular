f50a97f7cf573a0bb996abe71857afb7dbcfc6fb
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-04-04 11:44:23 +0530

    refactor: remove unused routes and Vercel configuration for task rewrites

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { LoginComponent } from './login/login.component';
-// import { HomeComponent } from './home/home.component';
-// import { ProjectComponent } from './project/project.component';
-// import { TaskComponent } from './task/task.component';
-// import { EditProjectComponent } from './edit-project/edit-project.component';
-// import { EditTaskComponent } from './edit-task/edit-task.component';
-
-// export const appRoutes: Routes = [
-//   { path: '', redirectTo: '/login', pathMatch: 'full' },
-//   { path: 'login', component: LoginComponent },
-//   { path: 'home', component: HomeComponent },
-//   { path: 'projects', component: ProjectComponent },
-//   { path: 'tasks/:projectTitle', component: TaskComponent },
-//   { path: 'projects/edit/:id', component: EditProjectComponent },
-
-//   { path: '**', redirectTo: 'projects' },
-// ];
-
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -30,11 +11,8 @@ export const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'projects', component: ProjectComponent },
-  {
-    path: 'tasks/:projectTitle',
-    component: TaskComponent,
-    data: { renderMode: 'client' }, // ⬅️ Add this line only here
-  },
+  { path: 'tasks/:projectTitle', component: TaskComponent },
   { path: 'projects/edit/:id', component: EditProjectComponent },
+
   { path: '**', redirectTo: 'projects' },
 ];
